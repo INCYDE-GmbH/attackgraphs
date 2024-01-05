f061d00215b76dc7b6891ec57252c5d643658dcf
Markus Heinrich <markus.heinrich@incyde.com>
2024-01-05 17:16:42 +0100

    some comments in translate.py

--- scripts/translate.py
+++ scripts/translate.py
@@ -29,26 +29,41 @@ def main():
         # ["../DE/Zugsicherung mit ETCS.drawio"                  , "../EN/ETCS train protection.drawio"]
     ]
 
+    # some labels to exclude from translation
     exclude = ["1", "2", "3", "4", "5", ""]
 
-
+    # dict to save the translations in
+    # format:
+    # de: en
     translations = {}
 
+    # load translations from file
+    # assume the following format:
+    # DE = EN
+    # ignore lines that do not have exatly one equal sign "="
     with open("translations.txt") as file:
         for line in file.readlines():
             split = line.split("=")
             if len(split) == 2:
                 translations[split[0].strip()] = split[1].strip()
 
+    # do it for all entries in graphs
     for graph in graphs:
         tree = ET.parse(graph[0])
         result = tree.getroot().findall(".//object[@label]")
 
         for node in result:
             label = node.attrib["label"].replace("\n", "\\n")
+
+            # exclude some labels that we do not need to translate
             if label not in exclude:
+
+                # distinguish between countermeasures and other nodes
+                # find countermeasures by their node color
                 mxcell = node.findall(".//mxCell[@style]")
                 if "fillColor=#DAE8FC" in mxcell[0].attrib["style"]:
+                    # countermeasures are translated line by line
+                    # therefore, split into lines, try to translate and then reconstruct the label
                     entries = label.split("\\n")
                     for index in range(len(entries)):
                         if entries[index] in translations:
@@ -57,11 +72,14 @@ def main():
                             print(entries[index])
                     node.attrib["label"] = "\n".join(entries)
                 else:
+                    # non-countermeasures are translated completly
+                    # assume linebreaks are "escaped" as \n in translation file
                     if label in translations:
                         node.attrib["label"] = translations[label].replace("\\n", "\n")
                     else:
                         print(label)
 
+        # save the tree to the English version of the file
         tree.write(graph[1], encoding="UTF-8")
 
 if __name__ == '__main__':
