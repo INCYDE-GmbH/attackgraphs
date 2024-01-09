659b5db1f7a032a0653eb65f7718c0da1a0bba1b
Markus Heinrich <markus.heinrich@incyde.com>
2024-01-09 11:51:59 +0100

    update readme

--- scripts/translate.py
+++ scripts/translate.py
@@ -6,31 +6,31 @@ import xml.etree.ElementTree as ET
 def main():
 
     graphs = [
-        # ["../DE/Automatisierte Ablaufsteuerung.drawio"         , "../EN/automated hump control.drawio"],
-        # ["../DE/Automatisierte Planung und Planprüfung.drawio" , "../EN/automated planning and plan review.drawio"],
-        # ["../DE/Cloud-Stellwerk.drawio"                        , "../EN/cloud interlocking.drawio"],
-        # ["../DE/Dezentrales Stellwerk.drawio"                  , "../EN/decentralized interlocking.drawio"],
-        # ["../DE/Emotions- und Absichtserkennung.drawio"        , "../EN/emotion and intent recognition.drawio"],
-        # ["../DE/Ferngesteuerte Fahrzeuge.drawio"               , "../EN/remote controlled rolling stock.drawio"],
-        # ["../DE/Güterwegemanagement.drawio"                    , "../EN/freight route management.drawio"],
+        ["../DE/Automatisierte Ablaufsteuerung.drawio"         , "../EN/automated hump control.drawio"],
+        ["../DE/Automatisierte Planung und Planprüfung.drawio" , "../EN/automated planning and plan review.drawio"],
+        ["../DE/Cloud-Stellwerk.drawio"                        , "../EN/cloud interlocking.drawio"],
+        ["../DE/Dezentrales Stellwerk.drawio"                  , "../EN/decentralized interlocking.drawio"],
+        ["../DE/Emotions- und Absichtserkennung.drawio"        , "../EN/emotion and intent recognition.drawio"],
+        ["../DE/Ferngesteuerte Fahrzeuge.drawio"               , "../EN/remote controlled rolling stock.drawio"],
+        ["../DE/Güterwegemanagement.drawio"                    , "../EN/freight route management.drawio"],
         ["../DE/Intelligente Instandhaltung.drawio"            , "../EN/predictive maintenance.drawio"],
-        # ["../DE/Intermodale Güterabfertigung.drawio"           , "../EN/intermodal freight handling.drawio"],
-        # ["../DE/Intermodale Reisekette.drawio"                 , "../EN/intermodal travel chain.drawio"],
-        # ["../DE/Kontaktlose Fahrscheinkontrolle.drawio"        , "../EN/contactless ticket control.drawio"],
-        # ["../DE/Optimierte Reise- und Preisgestaltung.drawio"  , "../EN/optimised travel and price arrangement.drawio"],
-        # ["../DE/Optimierung der Fahrgastwechselzeit.drawio"    , "../EN/optimization of passenger changeover time.drawio"],
-        # ["../DE/Personenidentifizierung.drawio"                , "../EN/identification of persons.drawio"],
-        # ["../DE/Reise- und Lebensmanagement.drawio"            , "../EN/travel and life management.drawio"],
-        # ["../DE/Reisendenlenkung.drawio"                       , "../EN/passenger guidance through the train station.drawio"],
-        # ["../DE/Steuernder Durchgriff der Disposition.drawio"  , "../EN/steering action for scheduling.drawio"],
-        # ["../DE/Virtuelles Kuppeln.drawio"                     , "../EN/virtual coupling.drawio"],
-        # ["../DE/Vollautomatisiertes Fahren.drawio"             , "../EN/fully automated driving.drawio"],
-        # ["../DE/Vorort-Informationen.drawio"                   , "../EN/on-site information.drawio"],
-        # ["../DE/Zugsicherung mit ETCS.drawio"                  , "../EN/ETCS train protection.drawio"]
+        ["../DE/Intermodale Güterabfertigung.drawio"           , "../EN/intermodal freight handling.drawio"],
+        ["../DE/Intermodale Reisekette.drawio"                 , "../EN/intermodal travel chain.drawio"],
+        ["../DE/Kontaktlose Fahrscheinkontrolle.drawio"        , "../EN/contactless ticket control.drawio"],
+        ["../DE/Optimierte Reise- und Preisgestaltung.drawio"  , "../EN/optimised travel and price arrangement.drawio"],
+        ["../DE/Optimierung der Fahrgastwechselzeit.drawio"    , "../EN/optimization of passenger changeover time.drawio"],
+        ["../DE/Personenidentifizierung.drawio"                , "../EN/identification of persons.drawio"],
+        ["../DE/Reise- und Lebensmanagement.drawio"            , "../EN/travel and life management.drawio"],
+        ["../DE/Reisendenlenkung.drawio"                       , "../EN/passenger guidance through the train station.drawio"],
+        ["../DE/Steuernder Durchgriff der Disposition.drawio"  , "../EN/steering action for scheduling.drawio"],
+        ["../DE/Virtuelles Kuppeln.drawio"                     , "../EN/virtual coupling.drawio"],
+        ["../DE/Vollautomatisiertes Fahren.drawio"             , "../EN/fully automated driving.drawio"],
+        ["../DE/Vorort-Informationen.drawio"                   , "../EN/on-site information.drawio"],
+        ["../DE/Zugsicherung mit ETCS.drawio"                  , "../EN/ETCS train protection.drawio"]
     ]
 
     # some labels to exclude from translation
-    exclude = ["1", "2", "3", "4", "5", ""]
+    exclude = ["1", "2", "3", "4", "5", "", "Verbinder"]
 
     # dict to save the translations in
     # format:
